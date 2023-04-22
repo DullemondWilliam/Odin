4c7a35fcaaa7c58dffc36fb7b75e8c995eca6d73
Petalousa <terrapetalousa@gmail.com>
2023-04-22 16:58:45 -0400

    Add chunking function.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 
 import openai
 import whisper
+import tiktoken
 
 # load openai key from keys.json
 try:
@@ -12,11 +13,46 @@ except Exception as e:
     print(e)
     exit(-1)
 
+MODEL = "gpt-3.5-turbo"
+MODEL_MAX_TOKENS = 4096
 
 def audio_to_text(filename):
     model = whisper.load_model("large-v2")
     result = model.transcribe(filename)
+    
+    text_chunks = chunk_text(result)
+    
     return (result["text"])
+
+
+def chunk_text(text_result, prompt_buffer=50, response_buffer=500):
+    '''
+    Takes result of whisper transcription and returns a list of text chunks.
+     
+    The size of these chunks are based on the MODEL and MODEL_MAX_TOKENS
+    such that prompt_buffer + number of tokens of chunk + response_buffer < MODEL_MAX_TOKENS
+    '''
+
+    chunks = []
+    _chunk_text = ''
+    chunk_tokens = prompt_buffer + response_buffer
+    encoder = tiktoken.encoding_for_model(MODEL)
+    for segment in text_result.get('segments'):
+        segment_text = segment.get('text')
+        segment_tokens = len(encoder.encode(segment_text, disallowed_special=()))
+
+        if chunk_tokens + segment_tokens > MODEL_MAX_TOKENS:
+            chunks.append(_chunk_text)
+            _chunk_text = ''
+            chunk_tokens = prompt_buffer + response_buffer
+        
+        chunk_tokens += segment_tokens
+        _chunk_text += segment_text
+
+    if len(_chunk_text) > 0:
+        chunks.append(_chunk_text)
+
+    return chunks
 
 
 def send_gpt_request(transcribed_text, mode):
@@ -30,7 +66,7 @@ def send_gpt_request(transcribed_text, mode):
     else:
         raise ValueError("Invalid mode")
     
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+    completion = openai.ChatCompletion.create(model=MODEL, messages=[{"role": "user", "content": prompt}])
     return completion.choices[0].message.content.strip()
 
 
